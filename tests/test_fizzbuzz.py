55fe8919ed29894b5eb63881ead39fb4585da0ad
Jhaigh94 <haigh.j@northeastern.edu>
2026-05-01 10:08:23 +0100

    Added multiple of 3

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -4,6 +4,9 @@ from fizzbuzz import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
     def test_no1_return_string(self):
         self.assertEqual(fizzbuzz(1),"1")
+    
+    def test_multiple_of3(self):
+        self.assertEqual(fizzbuzz(3),"Fizz")
 
 if __name__ == "__main__":
     unittest.main()
